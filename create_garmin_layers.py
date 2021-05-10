ea7dbeeb88d0339e176e2aa17e8409ced3e202c3
Efox <efox.vlckova@gmail.com>
2021-05-10 23:38:51 +0200

    update cursor added

--- create_garmin_layers.py
+++ create_garmin_layers.py
@@ -1,7 +1,8 @@
 import arcpy, os
+import pandas as pd
 
 # Set environment settings
-arcpy.env.workspace = r"C:\Users\elisk\Desktop\_git\garmin\test.gdb"
+arcpy.env.workspace = r"C:\Users\elisk\Desktop\_git\garmin\garmin.gdb"
 arcpy.env.overwriteOutput=True
 
 # Set the local variables
@@ -19,15 +20,35 @@ for file in os.listdir(in_folder):
     if ".csv" in file:
         text_file_list.append(file)
 
-
+path_list = [ ]
 for text_file in text_file_list:
-    in_table = os.path.join(in_folder,text_file)
-    name = text_file.strip(".tcx.csv")
-    print(name)
-    layer = arcpy.management.XYTableToPoint(in_table, name, x_coords, y_coords, z_coords, arcpy.SpatialReference(4326))
-    arcpy.PointsToLine_management(layer,"line"+name,"", "")
-    arcpy.Delete_management(layer)
     
-#arcpy.Merge(path_list,merged_fc)
+    in_table = os.path.join(in_folder,text_file)
+    
+    record = pd.read_csv(in_table)
+    activity = (record['sport'].unique())
+    
+    name = text_file.strip(".tcx.csv")
+    XYoutfc = os.path.join("in_memory",name)
+    print(name)
+    
+    layer_xy = arcpy.management.XYTableToPoint(in_table, XYoutfc, x_coords, y_coords, z_coords, arcpy.SpatialReference(4326))
+    
+    layer_line = arcpy.PointsToLine_management(layer_xy,"line"+name,"", "")
+    arcpy.AddField_management(layer_line, "activity", "TEXT", "", "", "", "", "", "", "")
+    arcpy.Delete_management(layer)
+    field = ['activity']
 
-#arcpy.Delete_management("in_memory")
+    with arcpy.da.UpdateCursor(layer_line,field) as cursor:
+        for row in cursor:
+            row = activity
+            cursor.updateRow(row)
+            
+    path_list.append(layer_line)
+
+
+arcpy.Merge_management(path_list,"merged_fc")
+
+    
+
+arcpy.Delete_management("in_memory")
